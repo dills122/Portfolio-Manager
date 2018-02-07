a79fcdb4d9f05e0c7e5eb1da7f4ca44c2774805f
dills122 <Dylan@wifi-38-118.nwpe.iup.edu>
2018-02-07 16:08:54 -0500

    Profile Node
    Added signout method, added user data to page, began formatting the component.

--- src/renderer/components/Dashboard.vue
+++ src/renderer/components/Dashboard.vue
@@ -1,8 +1,11 @@
 <template>
 	<div class="dashboard-container">
-	<div>
+	<div class="row">
 		<div class="left-bar">
 			<profile-node></profile-node>
+			<div style="height: 50px; width: 12px;">
+				Test
+			</div>
 		</div>
 		<div class="main-content">main</div>
 		<div class="right-bar">right</div>
@@ -16,26 +19,28 @@
 		width: 100%;
 		height: auto;
 
-		div {
+		.row {
 			display: flex;
 			flex-direction: row;
 			justify-content: space-evenly;
 		}
-
 		.left-bar {
 			display: flex;
+			flex-direction: column;
 			width: 25%;
 			min-height: 100vh;
 			background-color: #FFF;
 		}
 		.main-content {
 			display: flex;
+			flex-direction: column;
 			width: 50%;
 			min-height: 100vh;
 			background-color: #CCC;
 		}
 		.right-bar {
 			display: flex;
+			flex-direction: column;
 			width: 25%;
 			min-height: 100vh;
 			background-color: #EEE;
--- src/renderer/components/Profile-Node.vue
+++ src/renderer/components/Profile-Node.vue
@@ -1,17 +1,67 @@
 <template>
-	<div>
-		Profile Node
+	<div class="profile-node">
+		<div class="header-area">{{ name }}</div>
+		<div class="info-area"></div>
+		<div class="exit-area">
+			<button v-on:click="signOut">Sign Out</button>
+		</div>
 	</div>
 </template>
 
 <style type="text/css" lang="scss">
-	
+	.profile-node {
+		width: 92%;
+		height: auto;
+		margin: .5em auto;
+		background-color: #FFF;
+
+		.header-info {
+
+		}
+
+		.info-area {
+
+		}
+
+		.exit-area {
+
+		}
+	}
 </style>
 
 <script type="text/javascript">
-	import firebase from 'firebase'
+import firebase from 'firebase'
+import Router from '../router'
 
-	export default {
-		name: 'profile-node'
+export default {
+	data() {
+		return {
+			name: 'Test Name',
+			user: null
+		}
+	},
+	created() {
+		this.fetchData();
+	},
+	methods: {
+		//Gets the data from the authed user and add to node
+		fetchData() {
+			var authUsr = firebase.auth().currentUser;
+			if(authUsr) {
+				this.user = authUsr;
+				console.log(authUsr);
+				this.name = authUsr.email;
+			}
+		},
+		signOut() {
+			firebase.auth().signOut().then(function() {
+				//User Signs out successfully
+				console.log("Signed Out");
+				Router.push('/login')
+			}).catch(function(error) {
+				console.log(error);
+			});
+		}
 	}
+}
 </script>
