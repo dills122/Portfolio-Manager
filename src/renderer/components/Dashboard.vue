802484e9ca7dd8e0df58b43b6965b954a244c0c8
dills122 <Dylan@Dylans-MacBook-Pro-2.local>
2018-02-06 19:03:22 -0500

    Began Dashboard
    Started dashboard layout and created the profile node component.

--- src/renderer/components/Dashboard.vue
+++ src/renderer/components/Dashboard.vue
@@ -1,14 +1,54 @@
 <template>
+	<div class="dashboard-container">
 	<div>
-		<h4>Dashboard View</h4>
+		<div class="left-bar">
+			<profile-node></profile-node>
+		</div>
+		<div class="main-content">main</div>
+		<div class="right-bar">right</div>
 	</div>
-
+	<div class="footer"></div>
+	</div>
 </template>
 
-<style type="text/css">
-	
+<style type="text/css" lang="scss">
+	.dashboard-container {
+		width: 100%;
+		height: auto;
+
+		div {
+			display: flex;
+			flex-direction: row;
+			justify-content: space-evenly;
+		}
+
+		.left-bar {
+			display: flex;
+			width: 25%;
+			min-height: 100vh;
+			background-color: #FFF;
+		}
+		.main-content {
+			display: flex;
+			width: 50%;
+			min-height: 100vh;
+			background-color: #CCC;
+		}
+		.right-bar {
+			display: flex;
+			width: 25%;
+			min-height: 100vh;
+			background-color: #EEE;
+		}
+	}
 </style>
 
 <script type="text/javascript">
-	
+	import Profile from './Profile-Node.vue'
+  export default {
+  	
+    components: {
+      'profile-node': Profile
+    }   
+ }   
 </script>
---
+++ src/renderer/components/Profile-Node.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		Profile Node
+	</div>
+</template>
+
+<style type="text/css" lang="scss">
+	
+</style>
+
+<script type="text/javascript">
+	import firebase from 'firebase'
+
+	export default {
+		name: 'profile-node'
+	}
+</script>
